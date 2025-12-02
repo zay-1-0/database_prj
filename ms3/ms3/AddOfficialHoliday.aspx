d19fc16c0b3a5b354f5f766b52cc6e22903757a1
Marwan20050 <amarwan57@yahoo.com>
2025-12-03 01:39:43 +0200

    add official holiday

---
+++ ms3/ms3/AddOfficialHoliday.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddOfficialHoliday.aspx.cs" Inherits="ms3.AddOfficialHoliday" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: large;
+        }
+        .auto-style2 {
+            font-size: x-large;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p class="auto-style2">
+            <asp:Button ID="backButton" runat="server" onClick="goToAdminHomePage" Text="back" />
+        </p>
+        <p class="auto-style2">
+            Update Attendance
+        </p>
+        <p class="auto-style1">
+            Holiday ID</p>
+        <p class="auto-style1">
+            <asp:TextBox ID="emp_id" runat="server"></asp:TextBox>
+        </p>
+        <p class="auto-style1">
+            Start Date&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; End Date</p>
+        <p class="auto-style1">
+            <asp:TextBox ID="check_in" runat="server"></asp:TextBox>
+&nbsp;<asp:TextBox ID="Check_out" runat="server"></asp:TextBox>
+        </p>
+        <p class="auto-style1">
+            <asp:Button ID="AddButton" runat="server" OnClick="AddHoliday" Text="add" />
+        </p>
+        <p class="auto-style1">
+        <asp:Label ID="WarningLabel" runat="server" CssClass="auto-style1" ForeColor="Red" Visible="false"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
+
---
+++ ms3/ms3/AddOfficialHoliday.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ms3
+{
+    public partial class AddOfficialHoliday : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void AddHoliday(object sender, EventArgs e)
+        {
+            string connectionString = "YourConnectionStringHere"; // Replace with your actual connection string
+
+            try
+            {
+                // Get values from textboxes
+                string holidayName = emp_id.Text.Trim(); // Note: TextBox ID suggests it's for holiday name
+                string fromDateText = check_in.Text.Trim();
+                string toDateText = Check_out.Text.Trim();
+
+                // Validate inputs
+                if (string.IsNullOrEmpty(holidayName))
+                {
+                    ShowWarning("Please enter a Holiday Name.");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(fromDateText) || string.IsNullOrEmpty(toDateText))
+                {
+                    ShowWarning("Please enter both Start Date and End Date.");
+                    return;
+                }
+
+                DateTime fromDate;
+                DateTime toDate;
+
+                if (!DateTime.TryParse(fromDateText, out fromDate))
+                {
+                    ShowWarning("Please enter a valid Start Date (format: MM/DD/YYYY).");
+                    return;
+                }
+
+                if (!DateTime.TryParse(toDateText, out toDate))
+                {
+                    ShowWarning("Please enter a valid End Date (format: MM/DD/YYYY).");
+                    return;
+                }
+
+                if (fromDate > toDate)
+                {
+                    ShowWarning("Start Date cannot be after End Date.");
+                    return;
+                }
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand("Add_Holiday", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        // Add parameters
+                        command.Parameters.Add("@holiday_name", SqlDbType.VarChar, 50).Value = holidayName;
+                        command.Parameters.Add("@from_date", SqlDbType.Date).Value = fromDate;
+                        command.Parameters.Add("@to_date", SqlDbType.Date).Value = toDate;
+
+                      
+
+                        connection.Open();
+                        int rowsAffected = command.ExecuteNonQuery();
+
+
+                        if (rowsAffected > 0)
+                        {
+                            ShowWarning($"Holiday '{holidayName}' added successfully!", false); // false means success, not warning
+                            
+                        }
+                  
+                        else
+                        {
+                            ShowWarning("Holiday could not be added. Please check for overlapping dates or contact administrator.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowWarning($"Error: {ex.Message}");
+            }
+        }
+
+        private void ShowWarning(string message, bool isError = true)
+        {
+            WarningLabel.Text = message;
+            WarningLabel.ForeColor = isError ? System.Drawing.Color.Red : System.Drawing.Color.Green;
+            WarningLabel.Visible = true;
+        }
+
+       
+
+        protected void goToAdminHomePage(object sender, EventArgs e)
+        {
+            // Redirect to admin home page
+            Response.Redirect("AdminHomePage.aspx");
+        }
+    
+}
+}
