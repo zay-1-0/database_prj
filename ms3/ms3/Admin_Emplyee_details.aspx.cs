da9c00d09326ee8b3b02148ec3194666a316cb2e
Marwan20050 <amarwan57@yahoo.com>
2025-12-03 01:39:24 +0200

    employee details

---
+++ ms3/ms3/Admin_Emplyee_details.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ms3
+{
+    public partial class Admin_Emplyee_details : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                loadEmployeeData();
+            }
+        }
+
+        protected void goToAdminHomePage(object sender, EventArgs e)
+        {
+            Response.Redirect("Admin_HomePage.aspx");
+        }
+
+        private void loadEmployeeData()
+        {
+            try
+            {
+
+                string connStr = WebConfigurationManager
+                                 .ConnectionStrings["UniHR_DB"]
+                                 .ConnectionString;
+
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+
+                    string query = "SELECT * FROM allEmployeeProfiles";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+
+                    conn.Open();
+                    da.Fill(dt);
+
+                    Details_Table.DataSource = dt;
+                    Details_Table.DataBind();
+
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
